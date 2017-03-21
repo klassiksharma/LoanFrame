7cd5bde2c11dbe141fa98213c4fcf403ba53d655
klassiksharma <abksharma3@gmail.com>
2017-03-21 13:41:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var loanFrameApp = angular.module("LoanFrame", ["ngJsonExportExcel"]);
+
+var uniqueItems = function (data, key) {
+    var result = new Array();
+    for (var i = 0; i < data.length; i++) {
+        var value = data[i][key];
+ 
+        if (result.indexOf(value) == -1) {
+            result.push(value);
+        }
+    
+    }
+    return result;
+};
+
+loanFrameApp.filter('groupBy',
+            function () {
+                return function (collection, key) {
+                    if (collection === null) return;
+                    return uniqueItems(collection, key);
+        };
+    });
